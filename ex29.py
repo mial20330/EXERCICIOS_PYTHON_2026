05f4f126dcec8157a48570f03ae134fd5eb84d0d
Mialzin <candidohfabricio10@gmail.com>
2026-09-25 01:10:39 -0300

    Create ex29.py

---
+++ ex29.py
@@ -0,0 +1,9 @@
+print("multiplicação de 3 números \n")
+
+n1 = int(input("insira o primeiro número: "))
+n2 = int(input("insira o segundo número: "))
+n3 = int(input("insira o terceiro número: "))
+
+resul = n1 * n2 *  n3
+
+print(f"o resultado dessa multiplicação é {resul}")
